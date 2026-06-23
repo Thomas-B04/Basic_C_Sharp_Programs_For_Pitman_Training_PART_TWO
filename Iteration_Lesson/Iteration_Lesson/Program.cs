c5ac433e18adb0ae5c09322acdc14b84d0dbd1ba
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-06-23 13:41:34 +0100

    Add initial solution, project, and sample iteration code
    Initialized solution and C# project targeting .NET 10.0. Added Program.cs with commented examples demonstrating array iteration and conditional output.

---
+++ Iteration_Lesson/Iteration_Lesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] test_scores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < test_scores.Length; i++)
+        //{
+        //    if (test_scores[i] > 85)
+        //    {
+        //        Console.WriteLine($"Passing test score: {test_scores[i]}");
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        //for (int n = 0; n < names.Length; n++)
+        //{
+        //    Console.WriteLine(names[n]);
+        //}
+        //Console.ReadLine();
+
+    }
+}
