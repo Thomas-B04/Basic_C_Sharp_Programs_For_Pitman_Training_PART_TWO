55a1f2f5957997b25acd0b38e1640409b1e6d1ef
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-06-24 03:56:29 +0100

    Filter test scores and count passing results
    Added logic to filter test scores above 85 into a new list and print the count of passing scores. Included commented examples for iterating and filtering lists of scores and names.

--- Iteration_Lesson/Iteration_Lesson/Program.cs
+++ Iteration_Lesson/Iteration_Lesson/Program.cs
@@ -23,5 +23,39 @@
         //}
         //Console.ReadLine();
 
+        //List<int> test_scores = [98, 99, 85, 70, 82, 34, 91, 90, 94];
+
+        //foreach (int score in test_scores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine($"Passing test score: {score}");
+        //    }
+        //}
+
+        //List<string> names = ["Jesse", "Erik", "Daniel", "Adam"];
+
+        //foreach (string name in names)
+        //{
+        //    if (name == "Jesse")
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //}
+
+        List<int> test_scores = [98, 99, 85, 70, 82, 34, 91, 90, 94];
+        List<int> passing_scores = new List<int>();
+
+        foreach (int score in test_scores)
+        {
+            if (score > 85)
+            {
+                passing_scores.Add(score);
+            }
+        }
+
+        Console.WriteLine(passing_scores.Count);
+
     }
 }
+
