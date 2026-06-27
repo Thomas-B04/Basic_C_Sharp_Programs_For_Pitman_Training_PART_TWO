2ed1b0dbb325a04f4475a520ef3ffe7ca407c125
Thomas-B04 <Thomas_Bunyan@outlook.com>
2026-06-27 18:04:59 +0100

    First part of the assignment: Created an array and a method to have what the user types to be added to the end of the pre-made words in the array before printing them

---
+++ List_and_iteration_practice_assignment/List_and_iteration_practice_assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "apple ", "banana ", "cherry ", "date ", "elderberry " };
+        
+        Console.WriteLine("Enter some text to add to the end of each word:");
+        string userInput = Console.ReadLine() ?? "";
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] += userInput;
+        }
+
+        Console.WriteLine("\nUpdated words:");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
